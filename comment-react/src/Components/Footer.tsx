97131ef625827d05f6f5e61d0ef4421ef6170bfb
Petrik Diak <diak@petrik.hu>
2024-02-12 15:09:10 +0100

    first commit

---
+++ comment-react/src/Components/Footer.tsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return ( <div className='row'>
+    <div className="col-12">
+        <img src='./OIP.jpg' className='titleImg'/>
+    </div>
+</div>
+ );
+}
+ 
+export default Footer;
---
+++ comment-react/src/Components/Main.tsx
@@ -0,0 +1,22 @@
+const Main = () => {
+    return ( 
+        <div className='row'>
+            <div className="col-12">
+            <h1>
+            Blog post Title
+            </h1>
+            </div>
+            <div className="col-12">
+            <p>
+            A dinnye mindig téma a nyár derekától kezdve, hiszen az igazán forró hetek hagyományosan legkedveltebb slágergyümölcse. 
+            A dinnye azonban jó pár éve nem csak az édes íze és a remek szomjoltó hatása miatt téma, hanem az ára körüli balhék miatt is, bár biztosan nem érti mindenki, hogy miért is baj, ha olcsó a dinnye, hiszen olcsón vásárolni jó. 
+            Apropó vásárlás, ha már veszünk, hogy találhatjuk meg a legjobbat, legfinomabbat? Az is gyakran felmerül a dinnyebarátok körében, hogy nem lehetne-e még lejjebb vinni az árat, például otthoni termesztéssel? 
+            Megint mások azt firtatják rendszeresen, hogy a dinnye végül is gyümölcs vagy zöldség? 
+            Nos, most az összes kínzó kérdésre kompakt, közérthető választ kaphatnak!
+            </p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Main;
